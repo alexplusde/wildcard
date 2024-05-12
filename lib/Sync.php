9db5ded6d8b8bf19aad8d1b549a25b3d55bcc405
Alexander Walther <mail@alexplus.de>
2024-05-12 17:57:53 +0200

    update LICENSE

--- lib/Sync.php
+++ lib/Sync.php
@@ -6,10 +6,15 @@ use rex_config;
 
 class Sync
 {
+
+/*
+ * WIP
+
     public static function sync()
     {
-        /* Prüfe, ob die Datei neuer ist als der Zeitstempel in der Datenbank */
+        // Prüfe, ob die Datei neuer ist als der Zeitstempel in der Datenbank.
         if (date(rex_config::get('wildcard', 'syncdatestamp') >= date())) {
         }
     }
+*/
 }
