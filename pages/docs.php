85e885144596424fcb9fd8ac8f58213c629e060b
alxndr-w <alxndr-w@users.noreply.github.com>
2024-05-25 18:50:21 +0000

    Apply php-cs-fixer changes

--- pages/docs.php
+++ pages/docs.php
@@ -1,1 +1,1 @@
-<?=/** Standard-Methoden, um README.md in REDAXO auszugeben */ rex_view::title(rex_i18n::msg('wildcard_title'));
+<?= /** Standard-Methoden, um README.md in REDAXO auszugeben */ rex_view::title(rex_i18n::msg('wildcard_title'));
